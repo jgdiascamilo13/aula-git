2a6cddd851015a782735e651b35cb92385e728c3
02119149240 <02119149240@JIPA-LABIN02-25.ifro.local>
2018-04-04 17:30:54 -0400

    Aula 04/04/2018 Usando o hibernate

--- Grupo2/src/br/edu/ifro/App.java
+++ Grupo2/src/br/edu/ifro/App.java
@@ -5,11 +5,15 @@
  */
 package br.edu.ifro;
 
+import br.edu.ifro.modelo.Aluno;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  *
@@ -19,6 +23,18 @@ public class App extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("aula");
+        EntityManager em = emf.createEntityManager();
+        
+        em.getTransaction().begin();
+        Aluno aluno1 = new Aluno();
+        aluno1.setNome ("Fulano");
+        
+        em.persist(aluno1);
+        em.getTransaction().commit();
+        
+        
+        
         Parent root = FXMLLoader.load(getClass().getResource("MenuPrincipal.fxml"));
         
         Scene scene = new Scene(root);
