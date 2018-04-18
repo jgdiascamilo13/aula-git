30277df8438dd383934b4e50144f56e6fd03b9b5
00080339271 <00080339271@JIPA-LABIN02-26.ifro.local>
2018-04-18 16:21:54 -0400

    Enviando classe Registro

--- Grupo2/src/br/edu/ifro/modelo/Registro.java
+++ Grupo2/src/br/edu/ifro/modelo/Registro.java
@@ -14,6 +14,30 @@ public class Registro {
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private String nomeServidor, nomeAluno;
     private Time hora;  
+
+    public String getNomeServidor() {
+        return nomeServidor;
+    }
+
+    public void setNomeServidor(String nomeServidor) {
+        this.nomeServidor = nomeServidor;
+    }
+
+    public String getNomeAluno() {
+        return nomeAluno;
+    }
+
+    public void setNomeAluno(String nomeAluno) {
+        this.nomeAluno = nomeAluno;
+    }
+
+    public Time getHora() {
+        return hora;
+    }
+
+    public void setHora(Time hora) {
+        this.hora = hora;
+    }
 }
 
 
